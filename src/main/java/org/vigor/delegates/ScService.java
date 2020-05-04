0ef26ed35c852ecee2c8d0f21df10ca302e7e313
git-tut-ig <igor.vykhodtsev@inbox.ru>
2020-05-04 15:33:41 +0300

    Health checks

--- src/main/java/org/vigor/delegates/ScService.java
+++ src/main/java/org/vigor/delegates/ScService.java
@@ -1,6 +1,7 @@
 package org.vigor.delegates;
 
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,11 @@ import org.vigor.api.ScApi;
 import org.vigor.model.ServiceCenter;
 
 import javax.validation.Valid;
+import java.io.IOException;
+import java.lang.reflect.InaccessibleObjectException;
+import java.net.InetAddress;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -18,44 +23,107 @@ public class ScService implements ScApi {
 
     private Map<Long, ServiceCenter> storage = Collections.synchronizedMap(new HashMap<>());
     private AtomicLong generator = new AtomicLong(0);
+    private HttpHeaders headers;
+    private AtomicBoolean hcMig = new AtomicBoolean(true);
+    private AtomicBoolean hcLb = new AtomicBoolean(true);
+
+    public ScService() throws IOException {
+        String hostname = InetAddress.getLocalHost().getHostName();
+        headers = new HttpHeaders();
+        headers.set("X-HOST", hostname);
+    }
 
     @Override
     public ResponseEntity<ServiceCenter> addSc(@Valid ServiceCenter body) {
         long id = generator.incrementAndGet();
         body.setId(id);
         storage.put(id, body);
-        return ResponseEntity.status(HttpStatus.CREATED).body(body);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .headers(headers)
+                .body(body);
     }
 
     @Override
     public ResponseEntity<Void> deleteSc(Long id) {
         if (storage.containsKey(id)) {
             storage.remove(id);
-            return ResponseEntity.ok().build();
+            return ResponseEntity
+                    .ok()
+                    .headers(headers)
+                    .build();
         } else {
-            return ResponseEntity.notFound().build();
+            return ResponseEntity
+                    .notFound()
+                    .headers(headers)
+                    .build();
         }
     }
 
     @Override
     public ResponseEntity<ServiceCenter> getScById(Long id) {
         if (storage.containsKey(id)) {
-            return ResponseEntity.ok(storage.get(id));
+            return ResponseEntity
+                    .ok()
+                    .headers(headers)
+                    .body(storage.get(id));
         } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .headers(headers)
+                    .build();
         }
     }
 
     @Override
     public ResponseEntity<List<ServiceCenter>> listSc() {
         ArrayList<ServiceCenter> list = new ArrayList<>(storage.values());
-        return ResponseEntity.ok(list);
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .body(list);
     }
 
     @Override
     public ResponseEntity<ServiceCenter> updateSc(@Valid ServiceCenter body) {
         long id = body.getId();
         storage.put(id, body);
-        return ResponseEntity.ok(body);
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .body(body);
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteAllSc() {
+        storage.clear();
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .build();
+    }
+
+    @Override
+    public ResponseEntity<Void> hcLb() {
+        HttpStatus status = hcLb.get() ? HttpStatus.OK : HttpStatus.SERVICE_UNAVAILABLE;
+        return ResponseEntity.status(status).headers(headers).build();
+    }
+
+    @Override
+    public ResponseEntity<Void> hcMig() {
+        HttpStatus status = hcMig.get() ? HttpStatus.OK : HttpStatus.SERVICE_UNAVAILABLE;
+        return ResponseEntity.status(status).headers(headers).build();
+    }
+
+    @Override
+    public ResponseEntity<Void> triggerHcLb() {
+        hcLb.set(false);
+        return ResponseEntity.ok().headers(headers).build();
+    }
+
+    @Override
+    public ResponseEntity<Void> triggerHcMig() {
+        hcMig.set(false);
+        return ResponseEntity.ok().headers(headers).build();
     }
 }
